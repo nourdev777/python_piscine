f18b53907b532b725aa2cd6601de36160afa80dd
nourdev777 <nourdev777@gmail.com>
2025-03-23 19:47:51 +0400

    ex00

---
+++ ex00/Hello.py
@@ -0,0 +1,16 @@
+# python_piscine/ex00/Hello.py
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+
+ft_list[1] = "World!"
+ft_tuple = ("Hello", "UAE!")
+ft_set.remove("tutu!")
+ft_set.add("Abu Dhabi!")
+ft_dict["Hello"] = "42AbuDhabi!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)

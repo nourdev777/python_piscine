20aabff31be53f3f97e2115b4934fffd1e8d029c
nourdev777 <nourdev777@gmail.com>
2025-03-30 15:01:02 +0400

    ex02

---
+++ ex02/find_ft_type.py
@@ -0,0 +1,15 @@
+# python_piscine/ex02/find_ft_type.py
+def all_thing_is_obj(object: any) -> int:
+    if type(object) == list:
+        print(f"List : {type(object)}")
+    elif type(object) == tuple:
+        print(f"Tuple : {type(object)}")
+    elif type(object) == set:
+        print(f"Set : {type(object)}")
+    elif type(object) == dict:
+        print(f"Dict : {type(object)}")
+    elif type(object) == str:
+        print(f"{object} is in the kitchen : {type(object)}")
+    else:
+        print("Type not found")
+    return 42
